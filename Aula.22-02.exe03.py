323c8f8a5a71be7cf41fa41e44b7dd0b34249f21
danielchactoura <dchactoura@hotmail.com>
2016-02-24 16:31:33 -0300

    Update Aula.22-02.exe03.py

--- Aula.22-02.exe03.py
+++ Aula.22-02.exe03.py
@@ -1,30 +1,59 @@
+import sys
+
 def menu():
-    print('Escolha a sua opção:')
     print('(1) Somar')
     print('(2) Subtrair')
     print('(3) Multiplicar')
     print('(4) Dividir')
     print('(5) Limpar memória')
     print('(0) Sair do programa')
+    print('Escolha a sua opção:')
 
-def sum(a,b):
+def sum():
+    a = eval(input("Entre com o primeiro número:"))
+    b = eval(input("Entre com o segundo número:"))
     s = a + b
     print('O resultado é:', s)
 
-def sub(a,b):
+def sub():
+    a = eval(input("Entre com o primeiro número:"))
+    b = eval(input("Entre com o segundo número:"))
     t = a - b
     print('O resultado é:',t)
 
-def mult(a,b):
+def mult():
+    a = eval(input("Entre com o primeiro número:"))
+    b = eval(input("Entre com o segundo número:"))
     m = a * b
     print('O resultado é:',m)
 
-def div(a,b):
+def div():
+    a = eval(input("Entre com o primeiro número:"))
+    b = eval(input("Entre com o segundo número:"))
     d = a/b
     print('O resultado é:',d)
 
 def clean_mem():
-    
 
-a = eval(input("Entre com o primeiro número:"))
-b = eval(input("Entre com o segundo número:"))
+    print('desenvolvendo')
+
+def choose_menu(opt):
+    if opt == 1:
+        sum()
+    elif opt == 2:
+        sub()
+    elif opt == 3:
+        mult()
+    elif opt == 4:
+        div()
+    elif opt == 5:
+        clean_mem()
+    elif opt == 0:
+        sys.exit()
+    else:
+        print('Dê uma opção válida: 1, 2, 3, 4, 5 ou 0')
+    menu()
+
+
+menu()
+choose_menu(int(input()))
