63b4d1a66713606360b4eca42ea0b35c4a5c8792
danielchactoura <dchactoura@hotmail.com>
2016-03-09 18:11:50 -0300

    Added files via upload

---
+++ inverte_recursivo.py
@@ -0,0 +1,11 @@
+def inverte(m,a,j):
+    if j < 0:
+        return ''.join(aux)
+    else:
+        aux.append(m[j])
+        return inverte(m,aux,j-1)
+
+
+num = input('DIGITE UM NÚMERO, FDP:\n') + '\n'
+aux = []
+print(inverte(num,aux,len(num)-1))
